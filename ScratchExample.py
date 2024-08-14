dcdd2ced12c501d485ecc0cad6bd2788c3351aa7
BozarkV2 <136119486+BozarkV2@users.noreply.github.com>
2024-08-14 11:51:39 -0400

    Update ScratchExample.py

--- ScratchExample.py
+++ ScratchExample.py
@@ -2,26 +2,30 @@
 """
 Created on Mon May 27 17:16:44 2024
 
-@author: Samth
+@author: Bozark
 """
 
 import main as mn
 import TAplotter as TAplt
 import Import as imp
 
-DIRECTORY = r'C:\Users\Samth\Box\VWGroupShared\Data\MRL_108_20240515_OTA_AQDS\20240514_AQDS\water_pH5_27AQDS_magic_angle\Test'
-outDir = r'C:\Users\Samth\Box\VWGroupShared\Data\MRL_108_20240515_OTA_AQDS\20240514_AQDS\water_pH5_27AQDS_magic_angle\Test'
+DIRECTORY = r'C:\MRL_108\water_pH5_27AQDS_magic_angle'
+all_subdir=False
+KineticWvl = 500 #nm
+binw=1 #nm
+outDir = r'C:\MRL_108_20240515_OTA_AQDS\water_pH5_27AQDS_magic_angle\Out'
 PROBE = [350,700]
 
-AQDOTA = mn.main(DIRECTORY,False,reimport = True)
-Ave_AQD = mn.AveTA(AQDOTA,PROBE)
+AQDOTA = mn.main(DIRECTORY,all_subdir,reimport = True)
+Ave_AQD = mn.AveTA(AQDOTA,PROBE,t0_meth=Auto,kineticWVL=KineticWvl)
 GVD_AQD = mn.GVDcorr(Ave_AQD)
 
-GVD_AQD.KineticTrace(500, 1)
-mn.manyKinetic(AQDOTA,500,1)
+GVD_AQD.KineticTrace(KineticWvl, binw)
+mn.manyKinetic(AQDOTA,KineticWvl,binw)
 
 TAplt.plotTAdata(GVD_AQD,color_max=0.005,color_min=-0.005)
 
-results = mn.manyFitKinetic(AQDOTA, 500, 1)
-imp.saveFits(results, outDir, 'AQDFit500nm', 1)
-imp.saveKin(AQDOTA, outDir, 'AQD500nm', 500)
+num_comp=1
+results = mn.manyFitKinetic(AQDOTA, KineticWvl, num_comp)
+imp.saveFits(results, outDir, 'AQDFit500nm', num_comp)
+imp.saveKin(AQDOTA, outDir, 'AQD500nm', KineticWvl)
